ac160e47ac84593c32f6793f5c1bd1f27e732104
sigmator234xfgj <165550581+sigmator234xfgj@users.noreply.github.com>
2024-04-09 20:27:03 +0300

    Update name.py
    жесть

--- name.py
+++ name.py
@@ -21,7 +21,97 @@ class ImageProcessor:
         pixmapimage = pixmapimage.scaled(w, h)
         img_lbl.setPixmap(pixmapimage)
         img_lbl.show()
-        
+    def saveImage(self):
+        '''сохраняеет копию файла'''
+        path = os.path.join(workdir, self.save_dir)
+        if not(os.path.exists(path) or os.path.isdir(path)):
+            os.mkdir(path)
+        fullname = os.path.join(path, self.filename)
+
+        self.image.save(fullname)
+    def do_bw(self):
+        self.image = self.image.convert('L')
+        self.saveImage()
+        image_path = os.path.join(workdir, self.save_dir, self.filename)
+        self.showImage(image_path)
+
+
+
+
+#Создание приложени
+app = QApplication([])
+win = QWidget()
+win.resize(1000,780)
+win.setWindowTitle('Easy Editor')
+
+#Создаиние виджетов
+dir_btn = QPushButton('Папка')
+left_btn = QPushButton('Лево')
+right_btn = QPushButton('Право')
+mirror_btn = QPushButton('Зеркало')
+rez_btn = QPushButton('Резкость')
+bw_btn = QPushButton('Ч/Б')
+#-----------------------
+img_lbl = QLabel('Картинка')
+img_list = QListWidget()
+
+#Зоздание линий
+btns_line = QHBoxLayout()
+main_line = QHBoxLayout()
+v_line1 = QVBoxLayout()
+v_line2 = QVBoxLayout()
+
+btns_line.addWidget(left_btn)
+btns_line.addWidget(bw_btn)
+btns_line.addWidget(right_btn)
+btns_line.addWidget(mirror_btn)
+btns_line.addWidget(rez_btn)
+
+v_line1.addWidget(dir_btn)
+v_line1.addWidget(img_list)
+
+v_line2.addWidget(img_lbl)
+v_line2.addLayout(btns_line)
+
+main_line.addLayout(v_line1, 20)
+main_line.addLayout(v_line2, 80)
+
+win.setLayout(main_line)
+#Логика
+workdir = ''
+workImage = ImageProcessor()
+def filter(files, ext):
+    result = []
+    for filename in files:
+        for e in ext:
+            if filename.endswith(e):
+                result.append(filename)
+    return result
+def openFolder():
+    global workdir
+    ext =  ['.jpg','.png','jpeg','.bmp','gif']
+    workdir = QFileDialog.getExistingDirectory()
+    files_names = os.listdir(workdir)
+    filenames = filter(files_names, ext)
+    img_list.clear()
+    img_list.addItems(filenames)
+
+def showImage():
+    filename = img_list.currentItem().text()
+    workImage.loadImage(workdir, filename)
+    workImage.showImage(workImage.image_path)
+
+
+
+
+#конекты
+dir_btn.clicked.connect(openFolder)
+img_list.itemClicked.connect(showImage)
+bw_btn.clicked.connect(workImage.do_bw)
+
+#Запуск приложения
+win.show()
+app.exec_()
 
 
 
